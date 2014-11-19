c60fd864706ee9bcc5205e6597e2c1d3dda358d8
l-yara <l_yara@bigmir.net>
2014-11-19 23:32:48 +0000

    CV moved to notes

---
+++ CV/auto/Yaroslav_Lyakh.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook "Yaroslav_Lyakh"
+ (lambda ()
+    (TeX-run-style-hooks
+     "fancyhdr"
+     "hyperref"
+     "helvetica"
+     "latex2e"
+     "res10"
+     "res"
+     "")))
+
---
+++ CV/auto/_region_.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook "_region_"
+ (lambda ()
+    (TeX-run-style-hooks
+     "fancyhdr"
+     "hyperref"
+     "helvetica"
+     "latex2e"
+     "res10"
+     "res"
+     "")))
+
