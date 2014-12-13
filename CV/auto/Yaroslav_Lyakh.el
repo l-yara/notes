ce66804ccd909fbcec176eb4b63d9e43d74a1f47
l-yara <l_yara@bigmir.net>
2014-12-13 23:14:28 +0000

    format done (mostly)

--- CV/auto/Yaroslav_Lyakh.el
+++ CV/auto/Yaroslav_Lyakh.el
@@ -1,7 +1,19 @@
 (TeX-add-style-hook "Yaroslav_Lyakh"
  (lambda ()
+    (TeX-add-symbols
+     '("emp" 1)
+     '("profileItem" 2)
+     '("osection" 1)
+     '("jobdes" 4))
     (TeX-run-style-hooks
      "fancyhdr"
+     "enumitem"
+     "inputenc"
+     "utf8"
+     "fontenc"
+     "T1"
+     "scalefnt"
+     "needspace"
      "hyperref"
      "helvetica"
      "latex2e"
